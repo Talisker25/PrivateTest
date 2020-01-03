6bfa0ce1db676e8b9aa24e0764a6afaf8d191211
cth87 <cth87@DESKTOP-UJCBEHG>
2020-01-03 16:23:16 +0900

    수정띠

--- TestProject/src/test/testMain.java
+++ TestProject/src/test/testMain.java
@@ -5,6 +5,14 @@ public class testMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello-World");
+		
+		String test1 = "A";
+		String test2 = "B";
+		String test3 = "C";
+		
+		if(test2.equals("A")){
+			System.out.println("¾îÇã À½ Æ²·ÈÁö ¸ğ¾ä ºü²ô");
+		}
 	}
 
 }
